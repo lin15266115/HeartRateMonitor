ea6eb63192c06f9dc7d16df532e096ddd6b10bcc
lin1526615 <lin15266115@users.noreply.github.com>
2025-05-23 14:59:54 +0800

    build(HeartRateMonitor_5): 更新版本信息并优化构建脚本
    - 更新版本信息：将 frozen-name 从 "1.1.0-alpha" 修改为 "1.2.0-alpha"，frozen-version 从 1.001000 修改为 1.002000，index URL 更新为 v1.2.0-alpha
    - 优化构建脚本：添加文件复制功能，将 heart-rate.png、LICENSE 和 version.json 复制到构建目录
    - 修改项目名称：从 HeartRateMonitor_3 改为 HeartRateMonitor_5

--- build.bat
+++ build.bat
@@ -1,12 +1,29 @@
 setlocal
 
-set "PROJECT_NAME=HeartRateMonitor_3"
+set "PROJECT_NAME=HeartRateMonitor_5"
 
 pyinstaller ^
  --name "%PROJECT_NAME%" ^
- --add-data "LICENSE;." ^
  --windowed ^
  --clean ^
  __main__.py
+
+set SOURCE_FILE="heart-rate.png"
+set TARGET_DIR=".\dist\%PROJECT_NAME%\"
+
+echo 正在复制文件...
+copy %SOURCE_FILE% %TARGET_DIR%
+
+set SOURCE_FILE="LICENSE"
+copy %SOURCE_FILE% %TARGET_DIR%
+
+set SOURCE_FILE="version.json"
+copy %SOURCE_FILE% %TARGET_DIR%
+
+if %errorlevel% equ 0 (
+    echo 文件复制成功！
+) else (
+    echo 文件复制失败！
+)
 
 endlocal
